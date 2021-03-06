606193df20fa405c93b01b462ed6fd211bfa8281
coffmanjrp <coffmanjrp@gmail.com>
2021-03-05 21:14:01 -0800

    Add GraphQL queries to BookList

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,4 +1,27 @@
+import { gql, useQuery } from '@apollo/client';
+
+const getBookQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
 const BookList = () => {
+  const { data, error, loading } = useQuery(getBookQuery);
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
+
+  if (error) {
+    console.log(error);
+  }
+
+  console.log(data);
+
   return (
     <div>
       <ul className="book-list">
