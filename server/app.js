bfa00e4d46f748b54c7c92cdcef2569ac53cd9a8
coffmanjrp <coffmanjrp@gmail.com>
2021-03-05 21:10:58 -0800

    Add cors to allow cross-origin requests

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const schema = require('./schema/schema');
 const colors = require('colors');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+
+// Allow cross-origin request
+app.use(cors());
 
 mongoose.connect(process.env.DB_CONNECT_PATH, {
   useNewUrlParser: true,
