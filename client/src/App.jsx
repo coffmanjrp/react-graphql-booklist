b32b8ff981dcfa3d0db84703dbf7caf2e06decd2
coffmanjrp <coffmanjrp@gmail.com>
2021-03-05 19:42:23 -0800

    Setup Apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import BookList from './components/BookList';
 import './App.css';
 
+// Apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
-    <div className="main">
-      <h1>Book List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="main">
+        <h1>Book List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 };
 
