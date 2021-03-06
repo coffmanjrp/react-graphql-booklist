98fb09e7b986115f9d3f159b2210e80f83cc39db
coffmanjrp <coffmanjrp@gmail.com>
2021-03-05 17:32:20 -0800

    Connect server to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
+const colors = require('colors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.DB_CONNECT_PATH, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('connected to database'.green.bold);
+});
 
 app.use(
   '/graphql',
@@ -14,4 +26,6 @@ app.use(
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server Running On Port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server Running On Port ${PORT}`.yellow.bold)
+);
