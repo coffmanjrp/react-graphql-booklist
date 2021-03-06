7295cb0a6ab19a4019347cc7c310e5712c3bf351
coffmanjrp <coffmanjrp@gmail.com>
2021-03-05 16:50:05 -0800

    Set nested book query to author query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,6 +3,7 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLID,
+  GraphQLList,
   GraphQLSchema,
 } = require('graphql');
 const _ = require('lodash');
@@ -12,6 +13,9 @@ const books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 const authors = [
@@ -41,6 +45,12 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
