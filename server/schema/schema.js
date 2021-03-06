ebf91945c28ae912db627a61662a2979df285616
coffmanjrp <coffmanjrp@gmail.com>
2021-03-05 16:59:44 -0800

    Make books and authors list query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -67,14 +67,25 @@ const RootQuery = new GraphQLObjectType({
         return _.find(books, { id: args.id });
       },
     },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      },
+    },
     author: {
       type: AuthorType,
       args: {
         id: { type: GraphQLID },
       },
       resolve(parent, args) {
-        // code to get data from DB or other sources
         return _.find(authors, { id: args.id });
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       },
     },
   },
